7567f33781be2483c31f4b44f99f8a7ad0ed108f
herunchen <will.330@qq.com>
2018-12-19 10:09:34 +0800

    inital commit

---
+++ src/main/java/com/cqhot/app/CqHotApplication.java
@@ -0,0 +1,14 @@
+package com.cqhot.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CqHotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CqHotApplication.class, args);
+	}
+
+}
+
