a9c1b120abead7172b8cfff0732a427131c894dd
herunchen <will.330@qq.com>
2018-12-20 19:24:18 +0800

    mapper包扫描

--- src/main/java/com/cqhot/app/CqHotApplication.java
+++ src/main/java/com/cqhot/app/CqHotApplication.java
@@ -1,10 +1,12 @@
 package com.cqhot.app;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
+@MapperScan(basePackages= {"com.cqhot.app.dao"})
 public class CqHotApplication {
 
 	public static void main(String[] args) {
