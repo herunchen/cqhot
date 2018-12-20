167d5d1e3c8103534030ee1313e1064bda8b849a
herunchen <will.330@qq.com>
2018-12-20 19:23:33 +0800

    userCtroller

--- src/main/java/com/cqhot/app/controller/UserController.java
+++ src/main/java/com/cqhot/app/controller/UserController.java
@@ -1,13 +1,17 @@
 package com.cqhot.app.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.cqhot.app.entity.User;
 
 @Controller
 public class UserController {
 	
 	@RequestMapping("/login")
-	public String login() {
+	public String login(HttpSession session,User user) {
 		return "index.jsp";
 	}
 }
