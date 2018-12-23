cb974804e955c6bb901a00bc9e07bd26363bf8e8
herunchen <will.330@qq.com>
2018-12-23 12:08:39 +0800

    实体类setter方法修改

--- src/main/java/com/cqhot/app/entity/User.java
+++ src/main/java/com/cqhot/app/entity/User.java
@@ -40,7 +40,7 @@ public class User implements Serializable{
 	public String getUsername() {
 		return username;
 	}
-	public void setUserName(String username) {
+	public void setUsername(String username) {
 		this.username = username;
 	}
 	public String getName() {
